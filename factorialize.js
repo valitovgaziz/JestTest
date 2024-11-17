bcdbe783221a0b8fa9487ed989be8051b56d0219
valitovgaziz <valitovgaziz@gmail.com>
2024-11-18 02:00:10 +0500

    Grout tests

--- factorialize.js
+++ factorialize.js
@@ -4,12 +4,16 @@ function factorialize(number) {
     } else if (number != Math.trunc(number)) {
         throw new RangeError(`Factorials are only defined for integers`);
     } else {
-        let result = number;
-        while (number > 1) {
-            number--;
-            result *= number;
+        if (number == 0 || number == 1) {
+            return 1
+        } else {
+            let result = number;
+            while (number > 1) {
+                number--;
+                result *= number;
+            }
+            return result;
         }
-        return result;
     }
 }
 
